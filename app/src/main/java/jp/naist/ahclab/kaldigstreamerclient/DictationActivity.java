3c1d81f8b1158dfabec343f762880b77514ee4fd
王海峰 <haifeng@haifengMBP.local>
2016-10-27 15:55:21 +0800

    Format code; Fix Websocker URL issue; Update APP name

--- app/src/main/java/jp/naist/ahclab/kaldigstreamerclient/DictationActivity.java
+++ app/src/main/java/jp/naist/ahclab/kaldigstreamerclient/DictationActivity.java
@@ -14,41 +14,43 @@ import jp.naist.ahclab.speechkit.view.ListeningDialog;
 
 public class DictationActivity extends Activity {
 
-    ListeningDialog lst_dialog;
-    Button btn_start;
+  ListeningDialog lst_dialog;
+  Button btn_start;
 
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dictation);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_dictation);
 
-        lst_dialog = new ListeningDialog(DictationActivity.this);
-        btn_start = (Button)findViewById(R.id.btn_start);
-
-
-    };
+    lst_dialog = new ListeningDialog(DictationActivity.this);
+    btn_start = (Button) findViewById(R.id.btn_start);
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_dictation, menu);
-        return true;
+  }
+
+  ;
+
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.menu_dictation, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    int id = item.getItemId();
+
+    //noinspection SimplifiableIfStatement
+    if (id == R.id.action_settings) {
+      return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+    return super.onOptionsItemSelected(item);
+  }
 }
--- speechkit/src/main/java/jp/naist/ahclab/speechkit/SpeechKit.java
+++ speechkit/src/main/java/jp/naist/ahclab/speechkit/SpeechKit.java
@@ -3,35 +3,38 @@ package jp.naist.ahclab.speechkit;
 
 import android.content.Context;
 import android.util.Log;
+
 import jp.naist.ahclab.speechkit.Recognizer.Listener;
 
 public class SpeechKit {
-	private ServerInfo serverInfo;
-    private Context mcontext;
+  private ServerInfo serverInfo;
+  private Context mcontext;
 
-    static String TAG = "SpeechKit";
+  static String TAG = "SpeechKit";
 
-    public static enum Status {
-        INIT, RECOGNIZING, STOPPING, STOPPED, READY
-    }
-	public SpeechKit(Context mcontext, String appId, String appKey, ServerInfo serverInfo) {
-        this.serverInfo = serverInfo;
-        this.mcontext = mcontext;
-	}
+  public static enum Status {
+    INIT, RECOGNIZING, STOPPING, STOPPED, READY
+  }
 
-    public static SpeechKit initialize(Context applicationContext, String appId, String appKey, ServerInfo serverInfo) {
-        return new SpeechKit(applicationContext, appId, appKey, serverInfo);
-    }
-	
-	public void connect(){
-		
-	}
+  public SpeechKit(Context mcontext, String appId, String appKey, ServerInfo serverInfo) {
+    this.serverInfo = serverInfo;
+    this.mcontext = mcontext;
+  }
 
-	public Recognizer createRecognizer(Listener _listener) {
-		return new Recognizer(serverInfo,_listener);
-	}
-	public void setDefaultRecognizerPrompts(){
-		
-	}
+  public static SpeechKit initialize(Context applicationContext, String appId, String appKey, ServerInfo serverInfo) {
+    return new SpeechKit(applicationContext, appId, appKey, serverInfo);
+  }
+
+  public void connect() {
+
+  }
+
+  public Recognizer createRecognizer(Listener _listener) {
+    return new Recognizer(serverInfo, _listener);
+  }
+
+  public void setDefaultRecognizerPrompts() {
+
+  }
 
 }
